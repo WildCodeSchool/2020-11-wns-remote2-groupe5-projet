9f7f46f65f621bafe02cd5418f4b17a1a602374e
paul <paul.th.cartier@gmail.com>
2021-04-09 12:28:05 +0200

    fix refetch on homePage

--- web-client/src/components/pages/HomePage/HomePage.tsx
+++ web-client/src/components/pages/HomePage/HomePage.tsx
@@ -5,7 +5,7 @@ import { useQuery } from '@apollo/client';
 import { Articles_articles } from '../../../schemaTypes';
 
 export default function HomePage(): JSX.Element {
-  const { data } = useQuery(GET_ALL_ARTICLES);
+  const { data } = useQuery(GET_ALL_ARTICLES, { fetchPolicy: 'no-cache' });
   console.log('HomePage - data', data);
   return (
     <div className="lg:p-10 bg-gray-300 space-y-5 flex flex-col items-center justify-center">
