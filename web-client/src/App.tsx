dc084be5fab5c204903a1de2044561818e466368
guigzzz31 <guillaume.trouche.6@gmail.com>
2021-07-07 17:07:31 +0200

    font family

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -1,8 +1,9 @@
 import { useQuery } from '@apollo/client';
 import React from 'react';
-
+// import '@fontsource/quicksand.css';
 import CurrentUserContext from './contexts/CurrentUserContext';
 import { CHECK_AUTH } from './queries/user-queries';
+import '@fontsource/quicksand/600.css';
 
 import RootRouter from './router/RootRouter';
 
