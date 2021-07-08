e5dc45818502c37c279d19fc14673fc1104393d4
guigzzz31 <guillaume.trouche.6@gmail.com>
2021-07-08 17:34:25 +0200

    add community feature in front

--- web-client/src/components/SignIn/SignInCard.tsx
+++ web-client/src/components/SignIn/SignInCard.tsx
@@ -7,7 +7,15 @@ import SignInForm01 from './SignInForm01';
 import SignInForm02 from './SignInForm02';
 import SignInForm03 from './SignInForm03';
 import renderProgressBar from '../helpers/ProgressBar';
-import { Box, VStack,Text, Button, Flex, Checkbox, useToast } from '@chakra-ui/react';
+import {
+  Box,
+  VStack,
+  Text,
+  Button,
+  Flex,
+  Checkbox,
+  useToast,
+} from '@chakra-ui/react';
 
 export type User = {
   pseudo: string;
@@ -19,7 +27,7 @@ export type User = {
   city: string;
   bio: string;
   skillz: string;
-  community: string;
+  communities: string[];
 };
 
 const defaultUser: User = {
@@ -32,21 +40,32 @@ const defaultUser: User = {
   city: '',
   bio: '',
   skillz: '',
-  community: '',
+  communities: [],
 };
 
 export default function SignInCard(): JSX.Element {
   const [user, setUser] = useState<User>(defaultUser);
   const [redirect, setRedirect] = useState(false);
-  const [isPolicyAccepted, setIsPolicyAccepted] = useState(false)
+  const [isPolicyAccepted, setIsPolicyAccepted] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
   const [displaySignInCard, setDisplaySignInCard] = useState(0);
   const [signIn] = useMutation(SIGN_IN);
-  const toast = useToast()
-
+  const toast = useToast();
 
   const onUserChange = <P extends keyof User>(prop: P, value: User[P]) => {
     setUser({ ...user, [prop]: value });
+  };
+
+  const handleCommunities = (value: string) => {
+    if (!user.communities.includes(value))
+      setUser({ ...user, communities: [...user.communities, value] });
+  };
+
+  const onDeleteCommunity = (community: string) => {
+    setUser({
+      ...user,
+      communities: [...user.communities.filter((com) => com !== community)],
+    });
   };
 
   const submitForm = async () => {
@@ -62,12 +81,19 @@ export default function SignInCard(): JSX.Element {
             city: user.city,
             bio: user.bio,
           },
+          communities: [
+            ...user.communities.map((com) => {
+              return {
+                community: com,
+              };
+            }),
+          ],
         },
       });
       toast({
-        title: "Account created.",
+        title: 'Account created.',
         description: "We've created your account for you.",
-        status: "success",
+        status: 'success',
         duration: 9000,
         isClosable: true,
       });
@@ -110,7 +136,9 @@ export default function SignInCard(): JSX.Element {
       return (
         <SignInForm03
           user={user}
+          handleCommunities={handleCommunities}
           onUserChange={onUserChange}
+          onDeleteCommunity={onDeleteCommunity}
           onMinusSignInForm={onMinusSignInForm}
         />
       );
@@ -124,54 +152,73 @@ export default function SignInCard(): JSX.Element {
         onSubmit={(e) => {
           e.preventDefault();
           submitForm();
-        }}>
-      <VStack>
-      <Box>
-          <Text color="gray.700" fontSize="3xl" fontWeight="bold" textAlign="center">SIGN IN</Text>
-        </Box>
-        <Box width="100%" borderBottom="2px" borderBottomColor="gray.400" paddingBottom="24px">
-          <Link to="/">
-            <Text 
-              color="gray.500" 
-              textAlign="center" 
-              _hover={{color: "#FFF"}}
+        }}
+      >
+        <VStack>
+          <Box>
+            <Text
+              color="gray.700"
+              fontSize="3xl"
+              fontWeight="bold"
+              textAlign="center"
             >
-              Or LOG IN with credentials
-          </Text>
-          </Link>
-        </Box>
-        <Box width="100%">
+              SIGN IN
+            </Text>
+          </Box>
+          <Box
+            width="100%"
+            borderBottom="2px"
+            borderBottomColor="gray.400"
+            paddingBottom="24px"
+          >
+            <Link to="/">
+              <Text
+                color="gray.500"
+                textAlign="center"
+                _hover={{ color: '#FFF' }}
+              >
+                Or LOG IN with credentials
+              </Text>
+            </Link>
+          </Box>
+          <Box width="100%">
             {renderSignInForm()}
             {displaySignInCard === 2 ? (
               <>
-                    <Flex w="100%" justifyContent="start" alignItems="center" marginBottom="16px">
-                    <Checkbox
-                      id="customCheckLogin"
-                      type="checkbox"
-                      colorScheme="blackAlpha"
-                      onClick={() => setIsPolicyAccepted(!isPolicyAccepted)}
-                    />
-                    <Text fontSize="small" color="gray.500" pl="16px"> 
-                      Accepter la politique de confidentialité
-                    </Text>
+                <Flex
+                  w="100%"
+                  justifyContent="start"
+                  alignItems="center"
+                  marginBottom="16px"
+                >
+                  <Checkbox
+                    id="customCheckLogin"
+                    type="checkbox"
+                    colorScheme="blackAlpha"
+                    onClick={() => setIsPolicyAccepted(!isPolicyAccepted)}
+                  />
+                  <Text fontSize="small" color="gray.500" pl="16px">
+                    Accepter la politique de confidentialité
+                  </Text>
                 </Flex>
                 <Flex justify="center">
-                    <Button
-                      type="submit"
-                      text="log in"
-                      colorScheme="gray.100"
-                      _hover={{color:"yellow.200"}}
-                      backgroundColor="gray.900"
-                    >Log In
-                    </Button>
-              </Flex>
+                  <Button
+                    type="submit"
+                    text="log in"
+                    colorScheme="gray.100"
+                    _hover={{ color: 'yellow.200' }}
+                    backgroundColor="gray.900"
+                  >
+                    Log In
+                  </Button>
+                </Flex>
               </>
             ) : null}
-        </Box>
-            <Flex alignItems="center" justify="center" w="100%">
-              {renderProgressBar(displaySignInCard)}
-            </Flex>
-      </VStack>
+          </Box>
+          <Flex alignItems="center" justify="center" w="100%">
+            {renderProgressBar(displaySignInCard)}
+          </Flex>
+        </VStack>
       </form>
     </Box>
   );
